56bde37a1e27089265c0a4c7bc12c4750061721a
Alexandre Van de Sande <alex.vandesande@ethdev.com>
2017-07-12 15:56:01 -0300

    add main net contract

--- contract.sol
+++ contract.sol
@@ -2,10 +2,6 @@ pragma solidity ^0.4.11;
 contract TheButton {
     uint public lastPressed;
     event ButtonPress(address presser, uint pressTime, uint score);
-
-    function TheButton() {
-        lastPressed = now;
-    }
     
     /// @notice Press the button! 
     function press() {
